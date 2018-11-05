760159e8b24ebd1bb897815dea41a5fca6fd6d5b
Simon Claret <sclaret@gmail.com>
2018-11-05 15:01:15 -0500

    Set checksum

--- Formula/alacritty.rb
+++ Formula/alacritty.rb
@@ -2,7 +2,7 @@ class Alacritty < Formula
   desc "Cross-platform, GPU-accelerated terminal emulator"
   homepage "https://github.com/jwilm/alacritty"
   url "https://github.com/jwilm/alacritty/archive/v0.2.1.tar.gz"
-  sha256 "BADCHECKSUM"
+  sha256 "d335f09ba914faf8d8b2ba91a67672aab3acd1a3bb1528ec3d9339381697f6a1"
   depends_on "rust" => :build
 
   def install
--- README.md
+++ README.md
@@ -5,11 +5,17 @@ Homebrew formula for alacritty
 
 ## Usage
 
+``` bash
+$ brew tap sclaret/alacritty
+
+$ brew install alacritty
+```
 
 
 ## Security
 
 Download artifact from https://github.com/jwilm/alacritty/releases
+
 
 ```bash
 $ shasum -a 256 alacritty-0.2.1.tar.gz
